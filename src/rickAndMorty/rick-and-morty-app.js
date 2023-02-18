0f03d70cf94c2bb73697808cf880bae9d84ccfff
BattlerHaru <kronk.sdance@gmail.com>
2023-02-18 17:48:47 -0600

    first commit

---
+++ src/rickAndMorty/rick-and-morty-app.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const RickAndMortyApp = (element) => {
+  document.querySelector("#app-title").innerHTML = "Rick and Morty App";
+  element.innerHTML = "Loading...";
+};
